396ef829fb9990776a31269cc6155988c2e47cf7
Ritesh Zode <riteshzode1122@gmail.com>
2022-07-27 18:57:03 +0530

    resolve the issue of file handling

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 import random
 
 def primary():
+
   print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  with open("quotes.txt") as f:
+    quotes = f.readlines()
 
   last = last = len(quotes) - 1
   rnd = random.randint(0, last)
